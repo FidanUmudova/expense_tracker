3a939491ffa809db7e6a6ef19b41e794ab8884cd
FidanUmudova <fidanumudova05@gmail.com>
2026-07-29 23:05:01 +0400

    Brief description of your changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart'; // 1. Provider-i daxil edirik
 import 'models/expense_model.dart';
+import 'providers/counter_provider.dart'; // 2. Yaratdığımız Provider-i daxil edirik
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,13 @@ void main() async {
   // 'expenses' adında xana (box) açaq
   await Hive.openBox<Expense>('expenses');
 
-  runApp(const MyApp());
+  runApp(
+    // 3. runApp daxilində ChangeNotifierProvider ilə bükürük
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,8 +31,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(child: Text("Expense Tracker - Checkpoint 1")),
+        body: Center(child: Text("Expense Tracker - Checkpoint 3")),
       ),
     );
   }
